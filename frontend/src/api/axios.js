013dd8da4a462a2ca8bf028c04471cff863b5429
Vaibhav Ghai <ghaivaibhav07@gmail.com>
2026-08-15 23:49:10 +0530

    Connect frontend to deployed backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://smartdesk-backend-5ffq.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
